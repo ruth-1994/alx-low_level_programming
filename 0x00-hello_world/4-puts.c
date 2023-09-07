1d2a67581db1f3ce43a00d3eb6035aaa67c5ec30
ruth <ruthtilahun.t@gmail.com>
2023-09-07 13:05:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 include<stdio.h>
-/**
- * main -Entry point
- *
- * Return : Alway 0(Succes)
- * /
-
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
